626658e60f53d789a5550e58e52ad8afd587d34a
Phil <philfeiteng@gmail.com>
2020-11-20 14:22:41 -0800

    delete all code in MainActivity.java and activity_main.xml

--- app/src/main/java/com/bcit/comp3717/waffle/MainActivity.java
+++ app/src/main/java/com/bcit/comp3717/waffle/MainActivity.java
@@ -1,50 +1,9 @@
 package com.bcit.comp3717.waffle;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-
-// import com.bcit.comp3717.waffle.ui.login.EmailLoginActivity;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "GoogleActivity";
-    private static final int RC_SIGN_IN = 0;
-
-    SignInButton btnsignin;
-
-    // [START declare_auth]
-    private FirebaseAuth mAuth;
-    // [END declare_auth]
-
-//    private GoogleSignInClient mGoogleSignInClient;
 
 
     @Override
@@ -52,99 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnsignin = findViewById(R.id.sign_in_button);
-
-
-
-        btnsignin.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.sign_in_button:
-                        checkCurrentUser();
-                        break;
-                    // ...
-                }
-            }
-        });
-
-
-        // [START initialize_auth]
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-        // [END initialize_auth]
-
-
-
-
     }
 
-    public void checkCurrentUser() {
-        // [START check_current_user]
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            // User is signed in
-            Intent i = new Intent(MainActivity.this, SuccessCustomerLoggedIn.class);
-            startActivity(i);
-        } else {
-            // No user is signed in
-            createSignInIntent();
-        }
-        // [END check_current_user]
-    }
-
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-    }
-
-    public void createSignInIntent() {
-        // [START auth_fui_create_intent]
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build());
-
-        // Create and launch sign-in intent
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN);
-
-        // [END auth_fui_create_intent]
-    }
-
-    // [START auth_fui_result]
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                Intent i = new Intent(MainActivity.this, SuccessCustomerLoggedIn.class);
-                startActivity(i);
-                // ...
-            } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                Toast.makeText(MainActivity.this, "something went wrong.\n" + response.getError().getErrorCode(),
-                        Toast.LENGTH_LONG).show();
-                // ...
-            }
-        }
-    }
 }
-    // [END auth_fui_result]
 
 
