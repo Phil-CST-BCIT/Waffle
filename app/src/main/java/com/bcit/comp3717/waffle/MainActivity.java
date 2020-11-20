19840ac4aa7abce7ed3830ba767b8eab751473ce
Phil <philfeiteng@gmail.com>
2020-11-20 15:19:50 -0800

    add bundled ml-barcoding dependency

--- app/src/main/java/com/bcit/comp3717/waffle/MainActivity.java
+++ app/src/main/java/com/bcit/comp3717/waffle/MainActivity.java
@@ -1,9 +1,18 @@
 package com.bcit.comp3717.waffle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
 
 
     @Override
