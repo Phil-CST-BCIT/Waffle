542eb67daff3b68c48d77a7e586c587943e24f63
Phil <philfeiteng@gmail.com>
2020-11-22 15:57:12 -0800

    create a barcode option obj in main

--- app/src/main/java/com/bcit/comp3717/waffle/MainActivity.java
+++ app/src/main/java/com/bcit/comp3717/waffle/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BarcodeScannerOptions options =
+                new BarcodeScannerOptions.Builder()
+                        .setBarcodeFormats(
+                                Barcode.FORMAT_QR_CODE)
+                        .build();
 
     }
 
